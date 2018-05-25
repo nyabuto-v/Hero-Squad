4aec2c34508fd4f317ac65042a9f2e35be0224a5
Nyabuto Vice <nyabutovice@gmail.com>
2018-05-25 09:10:39 +0300

    passing the Hero tests

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -17,8 +17,20 @@ public class HeroTest {
 
   @Test
   public void Hero_instantiatesWithHeroAge_String() {
-    Hero myHero = new Hero("Vice", "26", "Wisdom", "Memory", 2);
+    Hero myHero = new Hero("Vice", "26", "Wisdom", "Memory", 3);
     assertEquals("26", myHero.getHeroAge());
+  }
+
+  @Test
+  public void Hero_instantiatesWithHeroSpecialPower_String() {
+    Hero myHero = new Hero("Vice", "26", "Wisdom", "Memory", 4);
+    assertEquals("Wisdom", myHero.getSpecialPower());
+  }
+
+  @Test
+  public void Hero_instantiatesWithHeroWeakness_String() {
+    Hero myHero = new Hero("Vice", "26", "Wisdom", "Memory", 5);
+    assertEquals("Memory", myHero.getHeroWeakness());
   }
 
 }
